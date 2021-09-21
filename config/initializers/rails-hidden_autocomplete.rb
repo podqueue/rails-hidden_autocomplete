4b3e7d5486ec5606e956ec85486d1f9f74122b61
Ryan Baumann <ryan.baumann@gmail.com>
2021-09-20 20:08:36 -0400

    Switch from Rails.application.reloader.to_prepare to ActiveSupport.on_load(:action_view)

--- config/initializers/rails-hidden_autocomplete.rb
+++ config/initializers/rails-hidden_autocomplete.rb
@@ -1,4 +1,4 @@
-Rails.application.reloader.to_prepare do
+ActiveSupport.on_load(:action_view) do
   ActionView::Helpers::DateTimeSelector.prepend Rails::HiddenAutocomplete::ActionView::Helpers::DateTimeSelector
   ActionView::Helpers::FormTagHelper.prepend Rails::HiddenAutocomplete::ActionView::Helpers::FormTagHelper
   ActionView::Helpers::Tags::HiddenField.prepend Rails::HiddenAutocomplete::ActionView::Helpers::Tags::HiddenField
